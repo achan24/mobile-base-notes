b49c8a8907da7d3d637efc17ddee53491309786e
achan24 <58598070+achan24@users.noreply.github.com>
2025-04-27 11:40:30 +0100

    thing

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth"
+
+declare module "next-auth" {
+  interface User {
+    /** custom role you attach in callbacks / DB */
+    role?: "admin" | "user" | string
+  }
+
+  interface Session {
+    user?: User
+  }
+
+  /** (optional) If you’re using next-auth middleware helpers */
+  interface NextAuthRequest {
+    auth?: Session
+  }
+}
