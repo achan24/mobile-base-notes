3f5a5f020e83de89bd2d28c844a4dd5ee0242e9f
achan24 <58598070+achan24@users.noreply.github.com>
2025-04-27 10:40:56 +0100

    create invite fix

--- admin/InviteCreate.tsx
+++ admin/InviteCreate.tsx
@@ -4,7 +4,7 @@ import { Create, SimpleForm, TextInput, SelectInput } from "react-admin"
 export default function InviteCreate() {
   return (
     <Create title="Send Invite">
-      <SimpleForm redirect={false}>
+      <SimpleForm>
         <TextInput source="email" required />
         <SelectInput source="role" choices={[
           { id: "user",  name: "User"  },
